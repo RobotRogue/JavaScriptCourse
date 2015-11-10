09fe08dbaf047f50a1add454f8d920986d15630a
Nelson Gill <robotrogue@hotmail.com>
2015-11-10 12:41:17 -0800

    Incremental save

--- objectsIntro.js
+++ objectsIntro.js
@@ -179,4 +179,161 @@ var name2 = susan.name;
 var age2 = susan.age;
 
 
-// 
+// Accessing Properties, Part 2
+/* In the last exercise, we accessed properties using what is known as dot notation.
+Good name, right? So to access a property, we use ObjectName.PropertyName (e.g., bob.name)
+
+In addition to dot notation, we can also access properties using bracket notation.
+In this case we use ObjectName["PropertyName"] to access the desired property.
+Note, we need " " around the property's name.
+
+Take a look at our next example object called dog.
+Notice on line 8 how we save the dog's species into a variable by accessing the species property of dog using bracket notation. */
+
+// Instructions
+// Use bracket notation to save the dog's weight and age into variables as well.
+
+// Take a look at our next example object, a dog
+var dog = {
+  species: "greyhound",
+  weight: 60,
+  age: 4
+};
+
+var species = dog["species"];
+// fill in the code to save the weight and age using bracket notation
+var weight = dog["weight"];
+var age = dog["age"];
+
+
+// Another Way to Create
+/* The method we've used to create objects uses object literal notation—that is, creating a new object with { } and defining properties within the brackets.
+
+Another way of creating objects without using the curly brackets { } is to use the keyword new. This is known as creating an object using a constructor.
+
+The new keyword creates an empty object when followed by Object(). The general syntax is: */
+
+var objectName = new Object();
+
+/* We then have to fill this object with properties and labels. How do we do that?
+Check out the creation of the object bob to see what we do. We create the name property for the object bob by using bob.name and assigning that to a value.
+Contrast this to how we define properties in lines 6-7 for the susan1 object. */
+
+// Instructions
+/* Inspect the susan1 object carefully and note the use of object literal notation.
+
+Use constructor notation to create susan2, which should have the same properties and values as susan1. */
+
+// Our bob object again, but made using a constructor this time
+var bob = new Object();
+bob.name = "Bob Smith";
+bob.age = 30;
+// Here is susan1, in literal notation
+var susan1 = {
+  name: "Susan Jordan",
+  age: 24
+};
+// Make a new susan2 object, using a constructor instead
+var susan2 = new Object();
+susan2.name = "Susan Jordan";
+susan2.age = 24;
+
+
+// Putting it all together
+/* We've learned how to make objects in two different ways. Both are valid, and you can use which one you prefer.
+
+Let's practice how to use both one more time. */
+
+//Instructions
+/* Use literal notation to finish the snoopy object.
+Remember literal notation is the one where we fill in { } with separate properties and values with colons.
+Each property is separated by a comma.
+
+snoopy should have two properties, a species of "beagle" and age of 10.
+
+Then make buddy, a 5 year-old golden retriever, using constructor notation.
+This notation involves using the key word new to create an empty object.
+Then we fill it in using dot notation. */
+
+// help us make snoopy using literal notation
+// Remember snoopy is a "beagle" and is 10 years old.
+var snoopy = {
+  species: "beagle",
+  age: 10
+};
+
+// help make buddy using constructor notation
+// buddy is a "golden retriever" and is 5 years old
+var buddy = new Object();
+buddy.species = "golden retriever";
+buddy.age = 5;
+
+
+// More Practice Making Objects
+// Nice job! Let's do one more example to get the hang of making objects with desired properties.
+
+// Instructions
+/* Create an object named 'bicycle' that has 3 properties:
+
+a speed of 0
+a gear of 1
+a frame_material of "carbon fiber" */
+
+var bicycle= {
+  speed: 0,
+  gear: 1,
+  frame_material: "carbon fiber"
+};
+
+
+// The Object Constructor
+/* We mentioned the term constructor back in section one, when we talked about making an object using the keyword new.
+A constructor is a way to create an object.
+
+When we write bob = new Object( ); we are using a built-in constructor called Object.
+This constructor is already defined by the JavaScript language and just makes an object with no properties or methods.
+
+This means we have to add our properties one at a time, just like we've been doing.
+To review, we've created bob using the constructor and defined the name property for you. */
+
+// Instructions
+// Finish making bob by defining the age property and setting it equal to 20
+
+// here we make bob using the Object constructor
+var bob = new Object();
+bob.name = "Bob Smith";
+// add bob's age here and set it equal to 20
+bob.age = 20;
+
+
+// Custom Constructors
+/* But this approach of adding in properties one at a time for every object is tedious!
+Instead of always using the boring Object constructor, we can make our own constructors.
+
+This way we can set the properties for an object right when it is created.
+So instead of using the Object constructor which is empty and has no properties, we can make our own constructors which have properties.
+
+To see how this works, look at our Person constructor in lines 1–4.
+This constructor is used to make Person objects.
+Notice it uses the keyword this to define the name and age properties and set them equal to the parameters given.
+
+Now we can use this constructor to make our good friends bob and susan in only one line each!
+Look at lines 7–8: once we have the constructor, it's way easier to make people because we can
+include their name and age as arguments to their respective constructors. */
+
+// Instructions
+// Practice using the constructor to make a new Person called george, whose full name is "George Washington" and age is 275.
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
+var george = new Person("George Washington", 275);
+
+
+// Try it Out!
