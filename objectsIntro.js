46b6c64950a6d47141f4b30a6c3133619505ecf2
Nelson Gill <robotrogue@hotmail.com>
2015-11-14 11:54:31 -0800

    Push local changes

--- objectsIntro.js
+++ objectsIntro.js
@@ -564,3 +564,74 @@ var bob = new Object();
 bob.name = "Bob Smith";
 // add bob's age here and set it equal to 20
 bob.age = 20;
+
+
+// Custom Constructors
+/* But this approach of adding in properties one at a time for every object is tedious!
+Instead of always using the boring Object constructor, we can make our own constructors.
+
+This way we can set the properties for an object right when it is created.
+So instead of using the Object constructor which is empty and has no properties, we can make our own constructors which have properties.
+
+To see how this works, look at our Person constructor in lines 1–4.
+This constructor is used to make Person objects.
+Notice it uses the keyword this to define the name and age properties and set them equal to the parameters given.
+
+Now we can use this constructor to make our good friends bob and susan in only one line each!
+Look at lines 7–8: once we have the constructor, it's way easier to make people because we can
+include their name and age as arguments to their respective constructors. */
+
+// Instructions
+// Practice using the constructor to make a new Person called george, whose full name is "George Washington" and age is 275.
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob and susan again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 25);
+// help us make george, whose name is "George Washington" and age is 275
+var george = new Person("George Washington", 275);
+
+
+// Try it Out!
+/* Let's look at another example and practice coding constructors. Here we have made a Cat constructor for you, with age and color properties.
+
+Why is this Cat constructor so cool? It means if we have many cats and wanted to create an object for each cat, we could just use this constructor with the properties already defined.
+
+This is much better than using the Object constructor which just gives us an empty object and needs us to define every property and value for each cat object we would create. */
+
+// Instructions
+// Finish the Dog constructor we have started on line 7. You can include whatever parameters and properties you want (age, name, breed, whatever you can think of!) Use the Cat constructor as an example.
+
+function Cat(age, color) {
+  this.age = age;
+  this.color = color;
+}
+
+// make a Dog constructor here
+function Dog(size, color, breed, gender) {
+  this.size = 'medium';
+  this.color = 'brindle';
+  this.breed = 'Boston';
+  this.gender = 'female';
+}
+
+
+// More Options
+/* In a constructor, we don't have to define all the properties using parameters.
+Look at our new Person example on line 1, and see how we set the species to be "Homo Sapiens" (line 4).
+This means that when we create any Person, their species will be "Homo Sapiens".
+In this way, the values associated with name and age are not yet assigned, but species will always have the same value.
+
+In this case, both sally and holden will have the same species of "Homo Sapiens", which makes sense because that is the same across all people. */
+
+// Instructions
+/* Create a new object called sally using the Person constructor. Her name is "Sally Bowles" and she is 39.
+Create another object called holden. His name is "Holden Caulfield" and he is 16.
+
+Edit the sentence printed out such that it includes the age of sally and holden respectively. */
+
+...
